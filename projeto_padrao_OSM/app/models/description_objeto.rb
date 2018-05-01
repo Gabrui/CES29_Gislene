1947dc918dbd0c44700172b46679abfc10c27380
DylanNS <dylan-ns@hotmail.com>
2018-05-01 16:35:18 -0300

    Update DescriptionObjeto
    É uma coleção de Property que representa a descrição do Objeto.

--- projeto_padrao_OSM/app/models/description_objeto.rb
+++ projeto_padrao_OSM/app/models/description_objeto.rb
@@ -1,2 +1,10 @@
-class DescriptionObjeto < ApplicationRecord
+class DescriptionObjeto < ApplicationRecord :: Model
+	#Representa a descricao do Objeto
+  #Colecao de Property
+	has_many :propertys	
+  
+  #Metodo para adicionar property em propertys
+	def << (property)
+	   @propertys << property
+        end
 end
