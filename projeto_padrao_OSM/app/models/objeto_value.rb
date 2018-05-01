e00a972366e9d95c8e19f812372de7958bad5727
DylanNS <dylan-ns@hotmail.com>
2018-05-01 16:33:32 -0300

    Update ObjetoValue
    def << (property)

--- projeto_padrao_OSM/app/models/objeto_value.rb
+++ projeto_padrao_OSM/app/models/objeto_value.rb
@@ -1,2 +1,10 @@
-class ObjetoValue < ApplicationRecord
+class ObjetoValue < ApplicationRecord :: Model
+	#Representa as caracteristicas do Objeto
+  #Colecao de Property
+	has_many :propertys	
+
+  #metodo para adicionar property em propertys
+	def << (property)
+	   @propertys << property
+        end
 end
