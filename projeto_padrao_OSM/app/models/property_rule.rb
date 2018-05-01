c76935b090b8a9288a144d909b020eead1758bee
DylanNS <dylan-ns@hotmail.com>
2018-05-01 16:23:44 -0300

    Update PropertyRule
    Adicionado regra de criação de Property

--- projeto_padrao_OSM/app/models/property_rule.rb
+++ projeto_padrao_OSM/app/models/property_rule.rb
@@ -1,2 +1,8 @@
-class PropertyRule < ApplicationRecord
+class PropertyRule < ApplicationRecord :: Model
+
+  #Regra de criação da Property
+  def create (type,value)
+    #criar Property
+    Property.new(type,value)
+  end
 end
