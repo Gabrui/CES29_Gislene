da5b3b92798843dd41e49d425635586cf60fef69
DylanNS <dylan-ns@hotmail.com>
2018-05-01 16:29:21 -0300

    Update TipoValue
    Coleção de Property que representam as caracteristicas do Tipo

--- projeto_padrao_OSM/app/models/tipo_value.rb
+++ projeto_padrao_OSM/app/models/tipo_value.rb
@@ -1,2 +1,10 @@
-class TipoValue < ApplicationRecord
+class TipoValue < ApplicationRecord :: Model
+	#Representa as caracteristicas do Tipo
+  #Colecao de Property
+	has_many :propertys	
+
+  #metodo para adicionar Property em propertys
+	def << (property)
+	   @propertys << property
+        end
 end
