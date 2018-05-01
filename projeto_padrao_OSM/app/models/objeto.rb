b1d88f8b318d5a77940f4697743f5489edbbe1fe
DylanNS <dylan-ns@hotmail.com>
2018-05-01 16:30:01 -0300

    Update Objeto
    def initialize(type,value,tipo)
    def name

--- projeto_padrao_OSM/app/models/objeto.rb
+++ projeto_padrao_OSM/app/models/objeto.rb
@@ -1,2 +1,28 @@
-class Objeto < ApplicationRecord
+class Objeto <  ApplicationRecord :: Model
+  
+  #Classe que representa a abstração Objeto
+  
+  #Todo Objeto tem value que é uma coleção de Property
+  has_one :objetoValues
+  
+  #Todo Objeto tem um ObjetoType
+  has_one :objetoType , dependent: :destroy
+  
+  #Todo Objeto pertence a um Tipo
+  belong_to :tipo 
+
+  #Validar existência dos atributos
+  validates_presence_of :objetoType , :value
+
+  def initialize(type,value,tipo)
+    super
+    @objetoValues = value
+    @objetoType = type
+    @tipo = tipo
+    self.save!
+  end
+
+  def name
+      @objetoType.name
+  end
 end
