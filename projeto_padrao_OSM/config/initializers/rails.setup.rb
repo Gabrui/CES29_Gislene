12dda0bc7fc1ea21c0e1ab8dc6393d822d7ebe9b
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2018-05-20 22:01:27 -0300

    Máquina virtual atualizada

--- projeto_padrao_OSM/config/initializers/rails.setup.rb
+++ projeto_padrao_OSM/config/initializers/rails.setup.rb
@@ -2,21 +2,16 @@ OpenStreetMap::Application.config.after_initialize do
 
   if Rails.env.development? && ActiveRecord::Base.connection.table_exists?(:client_applications)
 
-    unless webmaster = User.find_by_email("admin@gislene.com")
-      return
+    webmaster = User.find_by_email("admin@gislene.com")
+    
+    if webmaster
+      id = webmaster.client_applications.find_by_name("iD")
+      website = webmaster.client_applications.find_by_name("Web Site")
+      if id and website
+        OAUTH_KEY = website.key
+        ID_KEY = id.key
+      end
     end
-
-    unless id = webmaster.client_applications.find_by_name("iD")
-      return
-    end
-
-
-    unless website = webmaster.client_applications.find_by_name("Web Site")
-      return
-    end
-
-    OAUTH_KEY = website.key
-    ID_KEY = id.key
   end
 
 end
--- projeto_padrao_OSM/script/vagrant/setup/provision.sh
+++ projeto_padrao_OSM/script/vagrant/setup/provision.sh
@@ -1,56 +1,98 @@
 #!/usr/bin/env bash
 
-# abort on error
+# Se acontecer erro para tudo
 set -e
 
-# set locale to UTF-8 compatible. apologies to non-english speakers...
+# Locale compatível com UTF-8
 locale-gen en_GB.utf8
 update-locale LANG=en_GB.utf8 LC_ALL=en_GB.utf8
 export LANG=en_GB.utf8
 export LC_ALL=en_GB.utf8
 
-# make sure we have up-to-date packages
+# Atualiza os programas
 apt-get update
-
-# upgrade all packages
 apt-get upgrade -y
 
-# install packages as explained in INSTALL.md
-apt-get install -y ruby2.3 libruby2.3 ruby2.3-dev \
+# Baixa o necessário
+sudo apt-get install -y ruby2.3 libruby2.3 ruby2.3-dev \
                      libmagickwand-dev libxml2-dev libxslt1-dev nodejs \
                      apache2 apache2-dev build-essential git-core \
                      postgresql postgresql-contrib libpq-dev postgresql-server-dev-all \
-                     libsasl2-dev imagemagick
-gem2.3 install bundler 
+                     libsasl2-dev imagemagick \
+                     phantomjs
+sudo gem2.3 install bundler
 
-## install the bundle necessary for openstreetmap-website
-pushd /srv/openstreetmap-website
-# do bundle install as a convenience
+## Faz o bundle install
+pushd /srv/projeto
 sudo -u vagrant -H bundle install --retry=10 --jobs=2
-# create user and database for openstreetmap-website
+# Cria um usuário e o banco de dados caso necessário
 db_user_exists=`sudo -u postgres psql postgres -tAc "select 1 from pg_roles where rolname='vagrant'"`
 if [ "$db_user_exists" != "1" ]; then
 		sudo -u postgres createuser -s vagrant
-		sudo -u vagrant createdb -E UTF-8 -O vagrant openstreetmap
+		sudo -u vagrant createdb -E UTF-8 -O vagrant gislene
 		sudo -u vagrant createdb -E UTF-8 -O vagrant osm_test
-		# add btree_gist extension
-		sudo -u vagrant psql -c "create extension btree_gist" openstreetmap
+		# Adiciona a extensão btree_gist
+		sudo -u vagrant psql -c "create extension btree_gist" gislene
 		sudo -u vagrant psql -c "create extension btree_gist" osm_test
 fi
-# build and set up postgres extensions
+
+# Compila e adiciona as outras extensões
 pushd db/functions
 sudo -u vagrant make
-sudo -u vagrant psql openstreetmap -c "CREATE OR REPLACE FUNCTION maptile_for_point(int8, int8, int4) RETURNS int4 AS '/srv/openstreetmap-website/db/functions/libpgosm.so', 'maptile_for_point' LANGUAGE C STRICT"
-sudo -u vagrant psql openstreetmap -c "CREATE OR REPLACE FUNCTION tile_for_point(int4, int4) RETURNS int8 AS '/srv/openstreetmap-website/db/functions/libpgosm.so', 'tile_for_point' LANGUAGE C STRICT"
-sudo -u vagrant psql openstreetmap -c "CREATE OR REPLACE FUNCTION xid_to_int4(xid) RETURNS int4 AS '/srv/openstreetmap-website/db/functions/libpgosm.so', 'xid_to_int4' LANGUAGE C STRICT"
+sudo -u vagrant psql gislene -c "CREATE OR REPLACE FUNCTION maptile_for_point(int8, int8, int4) RETURNS int4 AS '/srv/projeto/db/functions/libpgosm.so', 'maptile_for_point' LANGUAGE C STRICT"
+sudo -u vagrant psql gislene -c "CREATE OR REPLACE FUNCTION tile_for_point(int4, int4) RETURNS int8 AS '/srv/projeto/db/functions/libpgosm.so', 'tile_for_point' LANGUAGE C STRICT"
+sudo -u vagrant psql gislene -c "CREATE OR REPLACE FUNCTION xid_to_int4(xid) RETURNS int4 AS '/srv/projeto/db/functions/libpgosm.so', 'xid_to_int4' LANGUAGE C STRICT"
 popd
-# set up sample configs
+
+# Copia as configurações padrão
 if [ ! -f config/database.yml ]; then
 		sudo -u vagrant cp config/example.database.yml config/database.yml
 fi
 if [ ! -f config/application.yml ]; then
-		sudo -u vagrant cp config/example.application.yml config/application.yml
+		sudo -u vagrant cp config/example2.application.yml config/application.yml
 fi
-# migrate the database to the latest version
+
+# Cria o esqueleto do banco de dados
 sudo -u vagrant rake db:migrate
+
+# Cria um usuário administrador padrão
+rails runner -e development cria_admin.rb
+
 popd
+
+
+FILE="clonarGit.sh"
+
+/bin/cat <<EOM >$FILE
+
+git clone --depth=1 https://github.com/lindemberglta/CES29_Gislene
+cd CES29_Gislene
+cd projeto_padrao_OSM
+bundle install
+
+cd db/functions
+make libpgosm.so
+cd ../..
+
+bundle exec rake db:create
+
+psql -d gislene -c "create extension btree_gist"
+psql -d gislene -c "CREATE FUNCTION maptile_for_point(int8, int8, int4) RETURNS int4 AS '`pwd`/db/functions/libpgosm', 'maptile_for_point' LANGUAGE C STRICT"
+psql -d gislene -c "CREATE FUNCTION tile_for_point(int4, int4) RETURNS int8 AS '`pwd`/db/functions/libpgosm', 'tile_for_point' LANGUAGE C STRICT"
+psql -d gislene -c "CREATE FUNCTION xid_to_int4(xid) RETURNS int4 AS '`pwd`/db/functions/libpgosm', 'xid_to_int4' LANGUAGE C STRICT"
+
+
+# Copia as configurações padrão
+if [ ! -f config/database.yml ]; then
+		cp config/example.database.yml config/database.yml
+fi
+if [ ! -f config/application.yml ]; then
+		cp config/example2.application.yml config/application.yml
+fi
+
+# Cria o esqueleto do banco de dados
+bundle exec rake db:migrate
+
+# Cria um usuário administrador padrão
+rails runner -e development cria_admin.rb
+EOM
