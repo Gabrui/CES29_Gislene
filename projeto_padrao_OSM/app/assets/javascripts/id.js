e19f81819ccadb4962e52024da1ab0e1f8c96a60
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2018-06-21 01:57:39 -0300

    Adaptação do iD para AOM, instanciação

--- projeto_padrao_OSM/app/assets/javascripts/id.js
+++ projeto_padrao_OSM/app/assets/javascripts/id.js
@@ -11,49 +11,122 @@ document.addEventListener("DOMContentLoaded", function() {
       'Please upgrade your browser to edit the map.';
     container.className = 'unsupported';
   } else {
-    var id = iD.Context()
-      .minEditableZoom(7)
-      .embed(true)
-      .assetPath("iD/")
-      .assetMap(JSON.parse(container.dataset.assetMap))
-      .locale(container.dataset.locale, container.dataset.localePath)
-      .preauth({
-        urlroot: location.protocol + "//" + location.host,
-        oauth_consumer_key: container.dataset.consumerKey,
-        oauth_secret: container.dataset.consumerSecret,
-        oauth_token: container.dataset.token,
-        oauth_token_secret: container.dataset.tokenSecret
+
+    fetch("/aom/json").then( function(response) {return response.json();} ).then(function(j) {
+      console.log(iD.data.presets);
+      console.log(j);
+      let presobj = {};
+      let primi = {
+        "area": {
+          "name": "Area",
+          "tags": {},
+          "geometry": ["area"]
+        },
+        "line": {
+            "name": "Line",
+            "tags": {},
+            "geometry": ["line"]
+        },
+        "point": {
+            "name": "Point",
+            "tags": {},
+            "geometry": ["point"]
+        },
+        "vertex": {
+            "name": "Vertex",
+            "tags": {},
+            "geometry": ["vertex"]
+        },
+        "relation": {
+            "name": "Relation",
+            "tags": {},
+            "geometry": ["relation"]
+        }
+      };
+      let atributoObj = {};
+      for (let i=0; i<j.length; i++) {
+        console.log(j[i].nome_tipo);
+        let nome = j[i].nome_tipo;
+        let tag = {};
+        tag[nome] = "*";
+        presobj[nome] = {"name": nome, "tags":tag, "geometry": ["point", "area", "line"], "fields":[]};
+        for (let k=0; k<j[i].atributos.length; k++) {
+          let atrib = j[i].atributos[k];
+          atributoObj[atrib.nome] = {
+            "key": atrib.nome,
+            "label": atrib.nome,
+            "placeholder": atrib.hint,
+            "type": "localized"
+          };
+          presobj[nome].fields.push(atrib.nome);
+        }
+      }
+
+      atributoObj["name"] = {
+        "key": "name",
+        "type": "localized",
+        "label": "Name",
+        "placeholder": "Nome próprio."
+      };
+
+      for (let i in primi) {
+        presobj[i] = primi[i];
+      }
+
+      //iD.data.presets = {};
+      //iD.data.presets["presets"]= primi; //presobj;
+      iD.data.presets = {
+        fields: atributoObj,
+        presets: presobj
+    };
+      console.log(iD.data.presets);
+
+      var id = iD.Context()
+        .minEditableZoom(7)
+        .embed(true)
+        .assetPath("iD/")
+        .assetMap(JSON.parse(container.dataset.assetMap))
+        .locale(container.dataset.locale, container.dataset.localePath)
+        .preauth({
+          urlroot: location.protocol + "//" + location.host,
+          oauth_consumer_key: container.dataset.consumerKey,
+          oauth_secret: container.dataset.consumerSecret,
+          oauth_token: container.dataset.token,
+          oauth_token_secret: container.dataset.tokenSecret
+        });
+      
+
+
+      id.map().on('move.embed', parent.$.throttle(250, function() {
+        if (id.inIntro()) return;
+        var zoom = ~~id.map().zoom(),
+          center = id.map().center(),
+          llz = { lon: center[0], lat: center[1], zoom: zoom };
+
+        parent.updateLinks(llz, zoom);
+
+        // Manually resolve URL to avoid iframe JS context weirdness.
+        // http://bl.ocks.org/jfirebaugh/5439412
+        var hash = parent.OSM.formatHash(llz);
+        if (hash !== parent.location.hash) {
+          parent.location.replace(parent.location.href.replace(/(#.*|$)/, hash));
+        }
+      }));
+
+      parent.$("body").on("click", "a.set_position", function (e) {
+        e.preventDefault();
+        var data = parent.$(this).data();
+
+        // 0ms timeout to avoid iframe JS context weirdness.
+        // http://bl.ocks.org/jfirebaugh/5439412
+        setTimeout(function() {
+          id.map().centerZoom(
+            [data.lon, data.lat],
+            Math.max(data.zoom || 15, 13));
+        }, 0);
       });
 
-    id.map().on('move.embed', parent.$.throttle(250, function() {
-      if (id.inIntro()) return;
-      var zoom = ~~id.map().zoom(),
-        center = id.map().center(),
-        llz = { lon: center[0], lat: center[1], zoom: zoom };
-
-      parent.updateLinks(llz, zoom);
-
-      // Manually resolve URL to avoid iframe JS context weirdness.
-      // http://bl.ocks.org/jfirebaugh/5439412
-      var hash = parent.OSM.formatHash(llz);
-      if (hash !== parent.location.hash) {
-        parent.location.replace(parent.location.href.replace(/(#.*|$)/, hash));
-      }
-    }));
-
-    parent.$("body").on("click", "a.set_position", function (e) {
-      e.preventDefault();
-      var data = parent.$(this).data();
-
-      // 0ms timeout to avoid iframe JS context weirdness.
-      // http://bl.ocks.org/jfirebaugh/5439412
-      setTimeout(function() {
-        id.map().centerZoom(
-          [data.lon, data.lat],
-          Math.max(data.zoom || 15, 13));
-      }, 0);
+      id.ui()(container);
     });
-
-    id.ui()(container);
   }
 });
